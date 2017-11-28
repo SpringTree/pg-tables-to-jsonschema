0fe83cbf5feac44c8cb6fabf3eb8682c36ce9362
Mark Doeswijk <mark@springtree.nl>
2017-11-28 12:17:42 +0100

    fix(json-schema): Support for v6 replacing schema.id with schema.$id

--- bin/cli.js
+++ bin/cli.js
@@ -11,15 +11,16 @@ var program = require( 'commander' );
 program
   .version( '0.0.1' )
   .usage( '[options]' )
-  .option( '--pg-host <value>',     'The postgresql host to connect to' )
-  .option( '--pg-port <n>',         'The postgresql host to connect to. Defaults to 5432' )
-  .option( '--pg-database <value>', 'The postgresql database to connect to' )
-  .option( '--pg-user <value>',     'The postgresql user to login with' )
-  .option( '--pg-password <value>', 'The postgresql password to login with' )
-  .option( '--pg-schema <value>',   'The postgresql schema to convert' )
-  .option( '-i, --indent [size]',   'The indent size in spaces. Default: 2' )
-  .option( '-o, --out [file]',      'Output folder. Default output is to STDOUT' )
-  .option( '-b, --base-url [url]',  'The optional base url for the schema id' )
+  .option( '--pg-host <value>',           'The postgresql host to connect to' )
+  .option( '--pg-port <n>',               'The postgresql host to connect to. Defaults to 5432' )
+  .option( '--pg-database <value>',       'The postgresql database to connect to' )
+  .option( '--pg-user <value>',           'The postgresql user to login with' )
+  .option( '--pg-password <value>',       'The postgresql password to login with' )
+  .option( '--pg-schema <value>',         'The postgresql schema to convert' )
+  .option( '-i, --indent [size]',         'The indent size in spaces. Default: 2' )
+  .option( '-o, --out [file]',            'Output folder. Default output is to STDOUT' )
+  .option( '-b, --base-url [url]',        'The optional base url for the schema id' )
+  .option( '-p, --additional-properties', 'Allow additonal properties on final schema. Set option to allow properties. Default: false' )
 
   .parse( process.argv );
 
@@ -56,7 +57,11 @@ converter( program )
     console.log( 'Writing schema files...' );
     _.each( schemas, function( schema, index )
     {
-      let filename = path.join( program.out, _.last( schema.id.split( '/' ) ) );
+      // Since json schema v6 .id is replaced by .$id
+      //
+      const schemaId = schema.id || schema.$id;
+
+      let filename = path.join( program.out, _.last( schemaId.split( '/' ) ) );
       fs.writeFileSync( filename, JSON.stringify( schema, null, _.padStart( '', program.indent, ' ' ) ) )
       console.log( 'File created: ' + filename );
     } )
